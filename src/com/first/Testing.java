2937c376be1e319e292068b4fa55e81363a31ed7
Adrish123 <adrish.au94@gmail.com>
2021-11-06 16:11:54 +0530

    Modifying files

--- src/com/first/Testing.java
+++ src/com/first/Testing.java
@@ -3,6 +3,9 @@ package com.first;
 public class Testing {
 
 	public static void main(String[] args) {
+		int a = 20, b = 20;
+		int c = a+b;
+		System.out.println("Result: " +c);
 		System.out.println("Testing");
 	}
 
